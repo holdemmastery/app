4cdc4876c62ae5c69ac643c8d3199db4b0d8637f
Alan Tao <alantao912@protonmail.ch>
2024-02-11 00:29:57 -0500

    Changed flex: 1

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -24,7 +24,7 @@ const GameScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    display: flex,
+    flex: 1,
     flexDirection: column,
     justifyContent: 'center',
     alignItems: 'center',
