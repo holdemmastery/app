0a5c17a8a4bf670347fc9e5519766508d6eb9389
Race Williams <race@racewilliams.com>
2024-02-11 02:11:13 -0500

    link pages to footer and change about to home

--- src/comps/Footer/Footer.js
+++ src/comps/Footer/Footer.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'; // Ensure you've installed this package
+import { useNavigation } from '@react-navigation/native';
 
-const Footer = ({ navigation }) => {
+const Footer = () => {
+  const navigation = useNavigation();
+  
   return (
     <View style={styles.footerContainer}>
-      <TouchableOpacity style={styles.footerItem}>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => navigation.navigate('HomeScreen')}
+      >
+        <Image
+          source={require('../../../assets/spade.png')}
+          style={styles.circle}
+        />
+        <Text style={styles.itemText}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => navigation.navigate('GameScreen')}
+      >
         <Image
           source={require('../../../assets/play.png')}
           style={styles.circle}
         />
         <Text style={styles.itemText}>Train</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.footerItem}>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => navigation.navigate('StatsScreen')}
+      >
         <Image
           source={require('../../../assets/stats.png')}
           style={styles.circle}
         />
         <Text style={styles.itemText}>Stats</Text>
       </TouchableOpacity>
-       <TouchableOpacity style={styles.footerItem}>
-        <Image
-          source={require('../../../assets/spade.png')}
-          style={styles.circle}
-        />
-        <Text style={styles.itemText}>About</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.footerItem}>
+      <TouchableOpacity
+        style={styles.footerItem}
+        onPress={() => navigation.navigate('SettingsScreen')}
+      >
         <Image
           source={require('../../../assets/settings.png')}
           style={styles.circle}
