c8b60656e7eb82505a1de4b3b54ef44044840f4e
Alan Tao <alantao912@protonmail.ch>
2024-02-11 00:26:57 -0500

    Begun work on Game Screen

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native'
 
 const GameScreen = () => {
   return (
     <View style={styles.container}>
       <Text>GameScreen</Text>
+
+      <div className="board-area">
+
+      </div>
+
+      <div className="footer">
+        <div className="board-cards"
+          data-hand="flow: horizontal; cards: AS, KS, QS, JS, 10S, 9H, 3H">
+        </div>
+        
+        <button className="showNextCard"/>
+      </div>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: flex,
+    flexDirection: column,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  footer: {
+
+  }
 });
 
 export default GameScreen;
