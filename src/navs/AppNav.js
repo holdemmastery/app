013db1a37496c2711e1ed283b146a3bb52df5201
Race Williams <race@racewilliams.com>
2024-02-11 00:04:00 -0500

    remove splash

--- src/navs/AppNav.js
+++ src/navs/AppNav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from '../screens/SplashScreen';
 import HomeScreen from '../screens/HomeScreen';
 import GameScreen from '../screens/GameScreen';
 import StatsScreen from '../screens/StatsScreen';
@@ -11,13 +10,10 @@ const Stack = createNativeStackNavigator();
 const AppNav = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="SplashScreen" component={SplashScreen} />
-      <Stack.Group>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="GameScreen" component={GameScreen} />
-        <Stack.Screen name="StatsScreen" component={StatsScreen} />
-        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-      </Stack.Group>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="GameScreen" component={GameScreen} />
+      <Stack.Screen name="StatsScreen" component={StatsScreen} />
+      <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
     </Stack.Navigator>
   );
 };
