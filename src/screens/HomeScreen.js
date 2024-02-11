39008b6ca6d99c7f642de943e316f290d6461209
Race Williams <race@racewilliams.com>
2024-02-11 00:28:29 -0500

    logo to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const HomeScreen = () => {
   return (
-    <LinearGradient colors={['#0f0c29', '#302b63', '#24243e']} style={styles.container}>
-    
+    <LinearGradient colors={['#002440', '#00000D']} style={styles.container}>
+      <Image
+        source={require('../../assets/centerpiece.png')}
+        style={styles.centerpiece}
+        resizeMode="contain"
+      />
     </LinearGradient>
   );
 };
@@ -13,8 +17,14 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     justifyContent: 'space-between',
   },
+  centerpiece: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 1520/2080,
+  }
 });
 
 export default HomeScreen;
